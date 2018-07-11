6b609a6430c8d3f763b7fc46a11a2f5ce173f936
DuParkLinux <dupark3@gmail.com>
2018-07-11 15:57:42 -0400

    Trying GeeksForGeeks solution. Both works

--- SpiralMatrix/spiralmatrix.py
+++ SpiralMatrix/spiralmatrix.py
@@ -1,4 +1,4 @@
-def spiralprint(matrix):
+def spiralPrint(matrix):
     """
     spiralprint takes a 2D matrix as input and prints 
     all elements in spiral form, going right, down, left, and up. 
@@ -60,8 +60,71 @@ def spiralprint(matrix):
                 row -= 1
     print() # print a newline after while loop is done
 
-matrix = [ [1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
-matrix2 = [ [1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17, 18]]
+matrix = [ [1, 2, 3, 4], 
+           [5, 6, 7, 8], 
+           [9, 10, 11, 12], 
+           [13, 14, 15, 16]]
 
-spiralprint(matrix)
-spiralprint(matrix2)
+matrix2 = [ [1, 2, 3, 4, 5, 6],
+            [7, 8, 9, 10, 11, 12],
+            [13, 14, 15, 16, 17, 18]]
+
+spiralPrint(matrix)
+spiralPrint(matrix2)
+
+
+def spiralPrintGFG(m, n, a) :
+    k = 0; l = 0
+ 
+    ''' k - starting row index
+        m - ending row index
+        l - starting column index
+        n - ending column index
+        i - iterator '''
+     
+ 
+    while (k < m and l < n) :
+         
+        # Print the first row from
+        # the remaining rows 
+        for i in range(l, n) :
+            print(a[k][i], end = " ")
+             
+        k += 1
+ 
+        # Print the last column from
+        # the remaining columns 
+        for i in range(k, m) :
+            print(a[i][n - 1], end = " ")
+             
+        n -= 1
+ 
+        # Print the last row from
+        # the remaining rows 
+        if ( k < m) :
+             
+            for i in range(n - 1, (l - 1), -1) :
+                print(a[m - 1][i], end = " ")
+             
+            m -= 1
+         
+        # Print the first column from
+        # the remaining columns 
+        if (l < n) :
+            for i in range(m - 1, k - 1, -1) :
+                print(a[i][l], end = " ")
+             
+            l += 1
+    print()
+    
+# Driver Code
+a = [ [1, 2, 3, 4, 5, 6],
+      [7, 8, 9, 10, 11, 12],
+      [13, 14, 15, 16, 17, 18],
+      [19, 20, 21, 22, 23, 24],
+      [25, 26, 27, 28, 29, 30],
+      [31, 32, 33, 34, 35, 36]]
+       
+R = 6; C = 6
+spiralPrintGFG(R, C, a)
+spiralPrint(a)
