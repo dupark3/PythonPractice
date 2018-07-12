1f83d42b1d5aba1440812b381c1e545c40e3614a
DuParkLinux <dupark3@gmail.com>
2018-07-12 08:07:35 -0400

    Using dynamic URLs using <variable_name> and converting that variable to a different type using <converter:variable_name>

--- Flask-Tutorial/hello.py
+++ Flask-Tutorial/hello.py
@@ -1,6 +1,22 @@
+# $ export FLASK_APP=hello.py           lets the terminal know to use flask app
+# $ flask run OR python -m flask run    optional flag --host=0.0.0.0 to listen to other IPs
+# $ export FLASK_ENV=development        code changes refreshed
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def greet():
-    return 'Hello, World!'
+    name = 'Du'
+    return 'Hello, ' + name + '!'
+
+# <variable_name> allows you to create dynamic URLs
+@app.route('/user/<username>')
+def show_profile(username):
+    return 'User name: %s' %username
+
+# <converter:variable_name> with no spaces allows you to convert from string type
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Viewing post #%d' %post_id
+
