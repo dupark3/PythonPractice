69573e2f8a0e9dc1d34ce1289bf4d5f4669befbc
DuParkLinux <dupark3@gmail.com>
2018-07-12 08:38:40 -0400

    Using url_for('function_name', optional arguments) and redirect() to dynamically redirect users

--- Flask-Tutorial/hello.py
+++ Flask-Tutorial/hello.py
@@ -2,21 +2,35 @@
 # $ flask run OR python -m flask run    optional flag --host=0.0.0.0 to listen to other IPs
 # $ export FLASK_ENV=development        code changes refreshed
 
-from flask import Flask
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def greet():
-    name = 'Du'
-    return 'Hello, ' + name + '!'
+def index():
+    return 'index'
 
 # <variable_name> allows you to create dynamic URLs
+@app.route('/login')
+def login():
+    return 'login'
+
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('login'))
+#     print(url_for('login', next='/'))
+#     print(url_for('profile', username='John%20Doe'))
+
+@app.route('/admin')
+def admin():
+    return 'Hello, admin!'
+
+@app.route('/guest/<guest_name>')
+def guest(guest_name):
+    return 'Hello, {} as Guest'.format(guest_name)
+
 @app.route('/user/<username>')
-def show_profile(username):
-    return 'User name: %s' %username
-
-# <converter:variable_name> with no spaces allows you to convert from string type
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return 'Viewing post #%d' %post_id
-
+def user(username):
+    if username == 'admin':
+        return redirect(url_for('admin'))
+    else:
+        return redirect(url_for('guest', guest_name = username))
