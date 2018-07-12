2c32538d5b69281799e10ab0d7c039b00eedac35
DuParkLinux <dupark3@gmail.com>
2018-07-12 14:46:23 -0400

    Using for loop in result.html using jinja templating.

--- Flask-Tutorial/hello.py
+++ Flask-Tutorial/hello.py
@@ -5,16 +5,20 @@
 from flask import Flask, redirect, url_for, request, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'index'
+    if request.method == 'POST':
+        return redirect(url_for('login'))
+    else:
+        return render_template('index.html')
 
 # <variable_name> allows you to create dynamic URLs
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         user=request.form['name']
-        return redirect(url_for('user', username=user))
+        age=request.form['age']
+        return redirect(url_for('user', username=user, age=age))
     else :
         return render_template('login.html')
 
@@ -22,13 +26,22 @@ def login():
 def admin():
     return 'Hello, admin!'
 
-@app.route('/guest/<guest_name>')
-def guest(guest_name):
-    return 'Hello, {} as Guest'.format(guest_name)
+@app.route('/guest/<guest_name>/<int:age>')
+def guest(guest_name, age):
+    return render_template('guest.html', jinjaname=guest_name, jinjaage=age)
 
-@app.route('/user/<username>')
-def user(username):
+@app.route('/user/<username>/<int:age>')
+def user(username, age):
     if username == 'admin':
         return redirect(url_for('admin'))
     else:
-        return redirect(url_for('guest', guest_name = username))
+        return redirect(url_for('guest', guest_name=username, age=age))
+
+@app.route('/result')
+def result():
+    dict = {'phys':60, 'chem':70, "math":90}
+    sum = 0.0
+    for key, val in dict.items():
+        sum += val
+    dict['avg'] = sum/len(dict)
+    return render_template('result.html', jinjadict=dict)
