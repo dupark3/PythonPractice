f2f44cadae4f834b6d1c78b9d5e8a6978772910b
DuParkLinux <dupark3@gmail.com>
2018-07-23 09:08:59 -0400

    Dictionary traversal and list to dict addition using inventory and loot

---
+++ AutomateTheBoringStuff/inventory.py
@@ -0,0 +1,19 @@
+def addToInventory(inventory, loot):
+    for item in loot:
+        inventory.setdefault(item, 0)
+        inventory[item] += 1
+
+def displayInventory(inventory):
+    print('Inventory: ')
+    total = 0
+    for k, v in inventory.items():
+        print(str(v) + ' ' + k)
+        total += v
+    print('Total number of items: ' + str(total))
+
+inventory = {'rope':42, 'coin':200, 'sword':1}
+displayInventory(inventory)
+
+dragonLoot = ['coin', 'rope', 'dagger', 'sword', 'coin', 'coin', 'armor']
+addToInventory(inventory, dragonLoot)
+displayInventory(inventory)
